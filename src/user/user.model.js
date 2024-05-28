bf4160a5035e18920fad502e23c6a1767a661131
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-05-28 16:08:58 -0600

    Se define el modelo de usuario

---
+++ src/user/user.model.js
@@ -0,0 +1,61 @@
+import mongoose from "mongoose";
+
+const UserSchema = mongoose.Schema({
+    name:{
+        type:String,
+        required:['name of user']
+    },
+    userName:{
+        type:String,
+        required:['username']
+    },
+    account:{
+        type:String,
+        required:['number of account']
+    },
+    DPI:{
+        type:String,
+        required:[`The user's personal identification document`]
+    },
+    address:{
+        type:String,
+        required:[`Provide the user's address`]
+    },
+    phone:{
+        type:Number,
+        required:['The number phone of user ']
+    },
+    email:{
+        type:String,
+        required:['The email of user']
+    },
+    password:{
+        type:String,
+        required:['The password email']
+    },
+    workPlace:{
+        type:String,
+        required:['Work of user']
+    },
+    salary:{
+        type:Number,
+        required:['Salary of user']
+    },
+    balance:{
+        type:Number,
+        required:['Balance of user']
+    },
+    role:{
+        type:String,
+        required:['Indicate whether the user is an administrator or a client'],
+        default: ['USER_ROLE']
+    }
+})
+
+UserSchema.methods.toJSON = function(){
+    const { __v, password, _id , ...user } = this.toObject();
+    this.uid = _id;
+    return user;
+}
+
+export default mongoose.model('User',UserSchema);
