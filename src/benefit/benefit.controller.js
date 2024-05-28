5d6a0d528b7e2e8055089fd52e3ba6d16edbb682
Egomez-2021340 <egomez-2021340@kinal.edu.gt>
2024-05-28 17:11:18 -0600

    se crean metodos agregar, listar y eliminar

--- src/benefit/benefit.controller.js
+++ src/benefit/benefit.controller.js
@@ -14,6 +14,60 @@ export const createBenefit = async (req, res) => {
         res.status(201).json(newBenefit);
     } catch (error) {
 
-        res.status(500).json({ message: error.message });
+        res.status(500).json({ error: 'Error when created a new benefit' });
+    }
+}
+
+export const getBenefits = async (req, res) => {
+
+    try {
+
+        const { nameBenefit, price, status } = req.query;
+
+        const filter = {};
+        if (nameBenefit) filter.nameBenefit = { $regex: nameBenefit, $option: 'i' };
+        if (price) filter.price = { $regex: price, $option: 'i' };
+        if (status !== undefined) filter.status = status;
+
+        const benefits = await Benefit.find(filter);
+
+        const total = benefits.length;
+
+        res.status(200).json({ total, benefits })
+    } catch (error) {
+
+        res.status(500).json({ error: 'Error getting benefits' })
+    }
+}
+
+export const getBenefitById = async (req, res) => {
+
+    try {
+
+        const { id } = req.params;
+
+        const benefit = await Benefit.findById(id);
+
+        if (!benefit) {
+
+            res.status(404).json({ error: 'Benefit not found' })
+        }
+
+        res.status(200).json(benefit)
+    } catch (error) {
+
+        res.status(500).json({ error: 'Error getting benefit details' })
+    }
+}
+
+export const deleteBenefit = async(req, res) => {
+
+    try{
+
+        const { id } = req.params;
+        
+    }catch(error){
+
+        res.status(500).json({ error: 'Error when deleting benefit'})
     }
 }
