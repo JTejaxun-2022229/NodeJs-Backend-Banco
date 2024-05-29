ff3fd218c8707978efddb201b8420b4af2dc803a
sramirez-2022506 <sramirez-2022506@kinal.edu.gt>
2024-05-29 17:26:02 -0600

    Se agrego el controlador y las rutas de trasnfer

---
+++ src/transfer/transfer.routes.js
@@ -0,0 +1,30 @@
+import {Router} from "express";
+import {check} from "express-validator";
+import {createTransfer, getTransfers, getTransferById, updateTransfer, deleteTransfer} from "./transfer.controller.js";
+import {validateJWT} from "../middlewares/validate-jwt.js";
+
+const router = Router()
+
+router.get('/', getTransfers)
+
+router.get('/:transferId',
+    [
+        check('transferId', 'The transfer ID is necesary').not().isEmpty()
+    ], getTransferById)
+
+router.post('/trasnfer',
+    [
+        validateJWT
+    ],createTransfer)
+
+router.put('/:transferId',
+    [
+        check('transferUpdate', 'The transfer ID is necesary').not().isEmpty()
+    ], updateTransfer)
+
+router.delete('/:trasnferId',
+    [
+        check('transferDelete', 'The transfer ID is necesary').not().isEmpty()
+    ], deleteTransfer)
+
+export default router 
