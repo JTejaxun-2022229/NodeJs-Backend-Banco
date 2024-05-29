3198eb443584f9f7c7b933f5d196ab83ab86f996
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-05-29 12:33:17 -0600

    Se cambian las respuestas de los validadores de espa;ol a ingles

--- src/helpers/db-validators.js
+++ src/helpers/db-validators.js
@@ -12,3 +12,16 @@ export const existeEmail = async (email = '') => {
         throw new Error(`Error al verificar la existencia del correo electrónico: ${error.message}`);
     }
 };
+
+export const existeDPI = async (DPI = '') => {
+    console.log('existeDPI',DPI)
+    try {
+        const user = await User.findOne({ DPI, state: true });
+        if (user) {
+            throw new Error(`El usuario con el DPI: ${DPI} ya existe`);
+            
+        }
+    } catch (error) {
+        throw new Error(`Error al verificar la existencia del DPI ${error.message}`);
+    }
+};
