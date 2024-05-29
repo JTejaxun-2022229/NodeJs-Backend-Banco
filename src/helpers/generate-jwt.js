fa4de04e30d6a3c249e17a758e12ed52824cf720
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-05-29 14:55:13 -0600

    Se agrega crear token y validar token

---
+++ src/helpers/generate-jwt.js
@@ -0,0 +1,16 @@
+import jwt from 'jsonwebtoken';
+import  token  from 'morgan';
+
+export const generarJWT = (uid = ' ',email = ' ') => {
+    return new Promise((resolve, reject) => {
+        const playload = ({uid,email});
+
+        jwt.sign(
+            playload,
+            process.env.SECRETORPRIVATEKEY,{expiresIn: '5h'},
+            (err,token) =>{
+                err ? (console.log(err), reject('Could not generate a token')) : resolve(token);
+            }
+        );
+    });
+}
