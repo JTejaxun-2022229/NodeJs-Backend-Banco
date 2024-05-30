656dbf890e59463f02cfed6e9404a5df9b35d6cd
Jose Tejaxun <jtejaxun-2022229@kinal.edu.gt>
2024-05-30 09:43:29 -0600

    fusion de favorite

---
+++ src/favorite/favorite.model.js
@@ -0,0 +1,34 @@
+import mongoose, { Schema } from 'mongoose';
+
+const FavoriteSchema = mongoose.Schema({
+    idUser: {
+        type: Number,
+        /*type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',*/
+        required: [true, "Enter a valid idUser"]
+    },
+    noAccount: {
+        type: Number,
+        required: true
+    },
+    DPI: {
+        type: String,
+        required: true
+    },
+    alias: {
+        type: String,
+        required: true
+    },
+    status:{
+        type: Boolean,
+        default: true
+    }
+});
+
+FavoriteSchema.methods.toJSON = function() {
+    const { __v, _id, ...favorite } = this.toObject();
+    favorite.favorite_id = _id;
+    return favorite;
+};
+
+export default mongoose.model('Favorite', FavoriteSchema);
--- src/helpers/db-validators.js
+++ src/helpers/db-validators.js
@@ -1,4 +1,6 @@
 import User from '../user/user.model.js';
+import Admin from "../admin/admin.model.js";
+import Favorite from '../favorite/favorite.model.js'
 
 export const existeEmail = async (email = '') => {
     console.log('existeEmail', email);
@@ -21,5 +23,28 @@ export const existeDPI = async (DPI = '') => {
         }
     } catch (error) {
         throw new Error(`Error verifying DPI existence: ${error.message}`);
+    }
+};
+
+/* Validación para que no se repita el correo en el post */
+export const existingAdminEmail = async (email = '') => {
+    const existEmail = await Admin.findOne({ email });
+    if (existEmail) {
+        throw new Error(`A admin with this email: ${email} is already in the database`);
+    }
+};
+
+/* Validación para ver si el ID ingresado es de algun admin */
+export const existeAdminById = async (id = '') => {
+    const existAdmin = await Admin.findById(id);
+    if (!existAdmin) {
+        throw new Error(`Admin with ID ${id} does not exist in the database!`);
+    }
+};
+
+export const existsFavoriteById = async (id = '') => {
+    const existFavorite = await Favorite.findById(id);
+    if (!existFavorite) {
+        throw new Error(`Favorite with ID ${id} does not exist in the database!`);
     }
 };
