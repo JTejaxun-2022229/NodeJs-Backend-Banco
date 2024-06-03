b9c03182567291dca4fc7d3d0c14288ab732a602
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-06-03 15:35:59 -0600

    Se realizan modificaciones a validar-token

--- src/middlewares/validar-jwt.js
+++ src/middlewares/validar-jwt.js
@@ -1,9 +1,8 @@
 import jwt from 'jsonwebtoken';
 import { request, response } from 'express';
-import User from '../user/user.model.js'
 
 export const validarJWT = async(req = request, res = response, next) => {
-    const token = req.header('x-token');
+    let token = req.body.token || req.query.token || req.headers['authorization']
 
     if(!token){
         return res.status(401).json({
@@ -13,23 +12,11 @@ export const validarJWT = async(req = request, res = response, next) => {
 
     try{
 
-        const {uid} = jwt.verify(token, process.env.SECRETPRIVATEKEY);
-        const user = await User.findById(uid);
+        token = token.replace(/^Bearer\s+/, '')
 
-        if(!user){
-            return res.status(401).json({
-                msg: 'User does not exist in the database'
-            })
-        }
+        const decoded = jwt.verify(token, process.env.TOKEN_KEY)
 
-        if(!user.state){
-            return res.status(401).json({
-                msg: "Invalid token, user in false state"
-            });
-        }
-
-        req.user = user;
-        next()
+        req.user = decoded  
 
     }catch(e){
         console.log(e);
@@ -37,4 +24,7 @@ export const validarJWT = async(req = request, res = response, next) => {
             msg: "This token is not valid"
         })
     }
+
+    return next()
+
 }
