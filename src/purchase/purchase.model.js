66d06568a2f39f21712c4d87839fa92a036c68dd
Jose Tejaxun <jtejaxun-2022229@kinal.edu.gt>
2024-07-09 22:57:13 -0600

    purchase funcional

--- src/purchase/purchase.model.js
+++ src/purchase/purchase.model.js
@@ -32,6 +32,11 @@ const PurchaseSchema = mongoose.Schema({
         type: Date,
         default: Date.now
     },
+    delivered: {
+        
+        type: Boolean,
+        default: false
+    },
     refunded: {
 
         type: Boolean,
