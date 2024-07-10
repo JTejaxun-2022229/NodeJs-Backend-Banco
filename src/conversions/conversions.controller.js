dc4ee14b960158c81ce2b87e23dada8ae8f83d9d
hgomez-2022207 <hgomez-2022207@kinal.edu.gt>
2024-07-10 01:41:43 -0600

    Se agregan las conversiones

--- src/conversions/conversions.controller.js
+++ src/conversions/conversions.controller.js
@@ -1,27 +1,38 @@
-import axios from 'axios'
+import axios from 'axios';
 
-export const convertData = async (req,res) =>{
-    const path = process.env.API_URL;
-    const key = process.env.EXCHANGE_RATE_API_KEY;
+export const convertData = async (req, res) => {
 
-    try{
-        const {from, to, amount} = req.body
-        const url = `${path}/${key}/pair/${from}/${to}/${amount}`
-        const response = await axios.get(url)
-        if(response.data && response.data.result === 'success'){
-            res.status(200).json({
+    console.log('convertData')
+    const apiUrl = process.env.API_URL;
+    const apiKey = process.env.EXCHANGE_RATE_API_KEY;
+
+    try {
+        const { from, to, amount } = req.body;
+
+        if (!from || !to || !amount) {
+            return res.status(400).json({ msg: 'Parámetros from, to o amount faltantes.' });
+        }
+
+        const url = `${apiUrl}/${apiKey}/pair/${from}/${to}/${amount}`;
+        const response = await axios.get(url);
+
+        console.log(url)
+
+        if (response.data && response.data.result === 'success') {
+            return res.status(200).json({
                 base: from,
                 target: to,
                 conversionRate: response.data.conversion_rate,
                 convertedAmount: response.data.conversion_result
-            })
-        }else{
-            res.status(400).json({
-                msg:'Error converting currency ', details: response.data
-            })
+            });
+        } else {
+            return res.status(400).json({
+                msg: 'Error al convertir la moneda',
+                details: response.data
+            });
         }
-    }catch(e){
-        console.log(e)
-        return res.status(500).json({msg: 'Error al realizar la conversión ', e})
+    } catch (error) {
+        console.error('Error al realizar la solicitud:', error);
+        return res.status(500).json({ msg: 'Error al realizar la conversión', error: error.message });
     }
-}
+};
